a24ef0394d7aabdb34fe861c578be770d19df079
Chai Phonbopit <phonbopit@gmail.com>
2018-02-05 03:38:59 +0700

    update eslint with standard

--- src/HelloApp.js
+++ src/HelloApp.js
@@ -1,32 +1,32 @@
 import React from 'react'
 
 class HelloApp extends React.Component {
-	constructor(props) {
+  constructor(props) {
     super(props)
 
     this.state = {
-    	counter: 0
+      counter: 0
     }
 
     this.handleClick = this.handleClick.bind(this)
-	}
+  }
 
-	handleClick() {
-		this.setState(prevState => ({
-			counter: prevState.counter + 1
-		}))
-	}
+  handleClick() {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }))
+  }
 
-	render() {
-		return (
+  render() {
+    return (
       <React.Fragment>
         <h1>Ahoy! React</h1>
         <p>{this.props.message}</p>
         <button onClick={this.handleClick}>Click me!</button>
         <p>Total click : {this.state.counter}</p>
       </React.Fragment>
-		)
-	}
+    )
+  }
 }
 
 export default HelloApp
