407483175dc11292f1e7ba39961f0b804c7ea6da
Chai Phonbopit <phonbopit@gmail.com>
2018-02-05 03:02:48 +0700

    finish step 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <HelloApp />
+        <HelloApp message="This is message sent from App" />
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
--- src/HelloApp.js
+++ src/HelloApp.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
 class HelloApp extends React.Component {
+	constructor(props) {
+    super(props)
+
+    this.state = {
+    	counter: 0
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+	}
+
+	handleClick() {
+		this.setState(prevState => ({
+			counter: prevState.counter + 1
+		}))
+	}
+
 	render() {
-		return <h1>Ahoy! React</h1>
+		return (
+      <React.Fragment>
+        <h1>Ahoy! React</h1>
+        <p>{this.props.message}</p>
+        <button onClick={this.handleClick}>Click me!</button>
+        <p>Total click : {this.state.counter}</p>
+      </React.Fragment>
+		)
 	}
 }
 
